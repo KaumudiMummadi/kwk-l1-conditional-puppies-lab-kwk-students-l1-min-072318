0340762bb8b496058f93692a3abca0d389c234ef
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-27 00:56:22 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -17,12 +17,12 @@ def less_puppies (number_of_puppies)
   elsif number_of_puppies == 3 
       return "Slow down! No more puppies."
   elsif number_of_puppies > 3
-      return "Get back on your animal restriction!"
+      return "Get back to your animal goals!"
   else 
       return "I love puppies"
 end   
 end 
- less_puppies(3)
+ less_puppies(4)
 
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
 
